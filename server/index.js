54494f8db8bc52ab1a4ff9ec7bb1a7487905fa31
maywoollcott <maywoollcott@gmail.com>
2021-03-11 18:16:29 -0500

    changes files to ts

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const PORT = 3001;
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
 app.use(router);
 
 (async function () {
